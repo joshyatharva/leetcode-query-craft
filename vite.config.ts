17add9bb19506e3ce8729a16877537910234408a
Atharva Joshi <atharvamj270800@gmail.com>
2025-05-25 19:42:27 +0530

    feat: allow github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -9,14 +9,13 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  plugins: [react(), mode === "development" && componentTagger()].filter(
+    Boolean
+  ),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: "leetcode-query-craft", // ← add this line
 }));
